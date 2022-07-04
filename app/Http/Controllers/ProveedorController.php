9868dd8561f5701f2d354aeb6030a8edd0574e7d
Manuel-Enrique-Gonzalez-Lagos-23 <megonzalezl@unah.hn>
2022-07-04 10:16:15 -0600

    Update

--- app/Http/Controllers/ProveedorController.php
+++ app/Http/Controllers/ProveedorController.php
@@ -5,7 +5,8 @@ use App\Models\Proveedor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\StoreProveedorRequest;
-use Illuminate\Support\Facades\DB;
+
+
 
 
 class ProveedorController extends Controller
