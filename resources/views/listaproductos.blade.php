678a9644d4b58efee104c2352eeaabeb873d70ed
Manuel-Enrique-Gonzalez-Lagos-23 <megonzalezl@unah.hn>
2022-07-04 13:20:04 -0600

    Actualización de inicio de sesión

---
+++ resources/views/listaproductos.blade.php
@@ -0,0 +1,81 @@
+@extends('plantilla.principalpag')
+@section('pestania', 'Lista de productos')
+@section('contenido')
+
+<style>
+tr:nth-child(even) {
+  background-color: #b3e0ff;
+}php
+</style>
+
+
+<style>
+td {
+    text-align: center;
+}
+</style>
+
+@if (session('Mensaje'))
+<div class="alert alert-danger">
+  {{session('Mensaje')}}
+</div>
+@endif
+
+</style>
+@if (session('msj'))
+<div class="alert alert-success">
+  {{session('msj')}}
+</div>
+@endif
+
+
+
+
+<h1 style="margin-left: 38% ; margin-top: 1%; margin-bottom: 3%; "> <u>Productos</u> </h1>
+
+
+
+<table  style="margin-top: 1%; width: 80%; margin-left: 4%;" >
+
+<tr style="background: #0088cc; text-align: center; border: 2px solid #dddddd;">
+<th>Nombre del proveedor</th>
+<th>Nombre del producto</th>
+<th>Principio activo</th>
+<th>Ver detalles</th>
+</tr>
+
+<tbody>
+    @if (count($produc)<=0)
+        <tr>
+            <td colspan="6">No hay resultados</td>
+    </tr>
+    @endif
+</tbody>
+
+@forelse($produc as $producto)
+
+<tr style="border: 2px solid #dddddd;">
+<td>{{$producto->Nombre_del_proveedor}}</td>
+<td>{{$producto->Nombre_del_distribuidor}}</td>
+<td>{{$producto->Principio_activo}}</td>
+<td > <a  class="btn btn-success" href="/Emple/{{$emple->id}}"> Detalles </a></td>
+</tr>
+
+@empty
+
+@endforelse
+
+
+{{$produc -> links() }}
+
+
+
+</tbody>
+
+</table>
+
+
+
+@section('pie_pagina', 'Copyright © 2022. FARMACIA LA POPULAR.')
+@endsection
+
