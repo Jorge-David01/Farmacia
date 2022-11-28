d1c85baeb72cc9e58fd21434ff72d2d7957e979b
Jorge David Cáceres <101753633+Jorge-David01@users.noreply.github.com>
2022-11-28 16:47:19 -0600

    Tablas cambios

--- resources/views/compra/detallescompra.blade.php
+++ resources/views/compra/detallescompra.blade.php
@@ -20,43 +20,44 @@
                 <div class="card">
 
                    <br>
-                    <table style="text-align: center; width: 96%; margin-left: 2%" class="table table-bordered align-items-center table-flush table-borderless table-responsive">
+                    <table style="text-align: center;  margin-left: 2%" class="table  align-items-center table-flush  table-responsive">
 
                         <tr style=" text-align: center; border: 2px solid #dddddd; margin-left: 5%;">
                             <th style="background: #0088cc; ">Número de factura:</th>
-                            <td style=" border: 2px solid #dddddd; margin-left: 1%">{{$comp->numero_factura}} </td> 
+                            <td style=" border: 2px solid #dddddd; margin-left: 1%">{{$comp->numero_factura}} </td>     
+                        </tr>
 
-                            <th style="background: #0088cc; margin-left: 5%;">Proveedor:</th>
+                        <tr style=" text-align: center; border: 2px solid #dddddd; margin-left: 5%;">
+                        <th style="background: #0088cc; margin-left: 5%;">Proveedor:</th>
                             <td style=" border: 2px solid #dddddd;">{{$comp->proveed->Nombre_del_proveedor}} </td>
+                        </tr>
 
-                            <th style="background: #0088cc; margin-left: 5%;">Fecha de pago:</th>
-                            <td>{{$comp->fecha_pago}} </td>
+                        <tr style=" text-align: center; border: 2px solid #dddddd; margin-left: 5%;">
+                        <th style="background: #0088cc; margin-left: 5%;">Fecha de pago:</th>
+                            <td style=" border: 2px solid #dddddd;">{{$comp->fecha_pago}} </td>
                         </tr>
 
                     </table>
 
                     <br>
 
+                    
+    
 
 
+                    <table style="text-align: center; width: 96%; margin-left: 2%; " class="table table-bordered align-items-center table-flush table-borderless table-responsive">
 
-                    <table style="text-align: center; width: 96%;margin-left: 2%" class="table table-bordered align-items-center table-flush table-borderless table-responsive">
 
-
-                        <tr style="background: #0088cc; text-align: center; border: 2px solid #dddddd;">
-                            <th>Nombre de producto</th>
+                        <tr style="background: #0088cc; text-align: center; border: 1px solid #dddddd; ">
+                            <th style="padding-right: 17px; padding-left: 17px;" >Nombre del producto</th>
+                            <th style="padding-right: 17px; padding-left: 17px;">Laboratorio</th>
+                            <th style="padding-right: 16.9px; padding-left: 16.9px;">Lotes</th>
+                            <th style="padding-right: 16.9px; padding-left: 16.9px;">Fecha de vencimiento</th>
+                            <th style="padding-right: 17px; padding-left: 17px;">Cantidad</th>                         
+                            <th style="padding-right: 17px; padding-left: 17px;">Precio de compra</th>
+                            <th style="padding-right: 17px; padding-left: 17px;">Precio de venta</th>
+                            <th style="padding-right: 17px; padding-left: 17px;">Total</th>
                            
-                            <th>Cantidad</th>
-                            
-                            <th>Precio de compra</th>
-                            <th>Precio de venta</th>
-                            <th>Total</th>
-
-                            <th>Laboratorio</th>
-
-                            <th>Lote</th>
-                            <th>Fecha de vencimiento</th>
-
                         </tr>
 
                         <?php
@@ -66,21 +67,21 @@
                         @forelse ($deta as $det)
 
                         <tr>
-                            <td>{{$det->nombre_producto}}</td>
-                            
-                            <td>{{$det->cantidad}}</td>
+                            <td style="padding-right: 16.9px; padding-left: 16.9px;">{{$det->nombre_producto}}</td>
+                            <td style="padding-right: 16.9px; padding-left: 16.9px;">{{$det->laboratorio}}</td>
+
+                            <td style="padding-right: 16.9px; padding-left: 16.9px;">{{$det->lote}}</td>
+                            <td style="padding-right: 16.9px; padding-left: 16.9px;">{{$det->fecha_vencimiento}}</td>
+                            <td style="padding-right: 16.9px; padding-left: 16.9px;">{{$det->cantidad}}</td>
                            
-                            <td>{{$det->precio_farmacia}}</td>
-                            <td>{{$det->precio_publico}}</td>
+                            <td style="padding-right: 16.9px; padding-left: 16.9px;">{{$det->precio_farmacia}}</td>
+                            <td style="padding-right: 16.9px; padding-left: 16.9px;">{{$det->precio_publico}}</td>
 
                             <?php $total = $det->precio_farmacia * $det->cantidad;
                             ?>
 
-                            <td> {{$total }} </td>
-                            <td>{{$det->laboratorio}}</td>
-
-                            <td>{{$det->lote}}</td>
-                            <td>{{$det->fecha_vencimiento}}</td>
+                            <td style="padding-right: 16.9px; padding-left: 16.9px;"> {{$total }} </td>
+                            
 
 
                         </tr>
@@ -105,79 +106,7 @@
 
 
 
-                    <br><br> 
-                    <hr>
-                    <br><br> 
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    <table style="text-align: center; width: 96%;margin-left: 2%" class="table table-bordered align-items-center table-flush table-borderless table-responsive">
-
-                        <tr style="background: #0088cc; text-align: center; border: 2px solid #dddddd;">
-                            <th scope="row">Número de factura:</th>
-                            <td style="background: #0088cc; border: 2px solid #dddddd;">{{$comp->numero_factura}} </td>
-                        </tr>
-
-                        <tr style="background: #0088cc; text-align: center; border: 2px solid #dddddd;">
-                            <th>Nombre de producto</th>
-                            <th scope="row">Proveedor:</th>
-                            <th>Cantidad</th>
-                            <th scope="row">Fecha de pago:</th>
-                            <th>Precio de compra</th>
-                            <th>Precio de venta</th>
-                            <th>Total</th>
-
-                            <th>Laboratorio</th>
-
-                            <th>Lote</th>
-                            <th>Fecha de vencimiento</th>
-
-                        </tr>
-
-                        <?php
-                        $total = 0;
-                        ?>
-
-                        @forelse ($deta as $det)
-
-                        <tr>
-                            <td>{{$det->nombre_producto}}</td>
-                            <td>{{$comp->proveed->Nombre_del_proveedor}}</td>
-                            <td>{{$det->cantidad}}</td>
-                            <td>{{$comp->fecha_pago}}</td>
-                            <td>{{$det->precio_farmacia}}</td>
-                            <td>{{$det->precio_publico}}</td>
-
-                            <?php $total = $det->precio_farmacia * $det->cantidad;
-                            ?>
-
-                            <td> {{$total }} </td>
-                            <td>{{$det->laboratorio}}</td>
-
-                            <td>{{$det->lote}}</td>
-                            <td>{{$det->fecha_vencimiento}}</td>
-
-
-                        </tr>
-
-                        @empty
-                        @endforelse
-
-                        <?php
-                        $total_suma = 0;
-                        ?>
-
-                    </table>
+                    
 
 
 
